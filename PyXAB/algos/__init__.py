9a1366b004a96cafc7b816d0f78abae3f639a1a1
WilliamLi <williamleewj@gmail.com>
2022-03-23 16:31:43 -0400

    added run_synthetic

---
+++ PyXAB/algos/__init__.py
@@ -0,0 +1,1 @@
+__all__ = ["GPO", "HCT", "HOO", "PCT", "POO", "VHCT", "VPCT"]
---
+++ PyXAB/run_synthetic.py
@@ -0,0 +1,45 @@
+from PyXAB.synthetic_obj import *
+from PyXAB.algos import *
+
+from PyXAB.partition.BinaryPartition import BinaryPartition
+from PyXAB.utils.plot import compare_regret
+import numpy as np
+
+
+def main(algo_list, target, domain, partition, noise=0.1, rounds=1000):
+    algo_dictionary = {'T-HOO': HOO.T_HOO(rounds=rounds, domain=domain, partition=partition),
+                       'HCT': HCT.HCT(domain=domain, partition=partition),
+                       'VHCT': VHCT.VHCT(domain=domain, partition=partition),
+                       'POO': POO.POO(domain=domain, partition=partition, algo=HOO.T_HOO),
+                       'PCT': PCT.PCT(domain=domain, partition=partition)}
+
+    results_dictionary = {}
+    for name in algo_list:
+        print(name, ": training")
+        algo = algo_dictionary[name]
+        regret_list = []
+        regret = 0
+        for t in range(1, rounds + 1):
+
+            print(t)
+            point = algo.pull(t)
+            reward = target.f(point) + np.random.uniform(-noise, noise)
+            algo.receive_reward(t, reward)
+            inst_regret = target.fmax - target.f(point)
+            regret += inst_regret
+            regret_list.append(regret)
+
+        results_dictionary[name] = np.array(regret_list)
+
+    return results_dictionary
+
+
+target = Garland.Garland()
+domain = [[0, 1]]
+partition = BinaryPartition
+rounds = 5000
+noise = 0.1
+
+algo_list = ['T-HOO', 'POO', 'HCT', 'PCT',  'VHCT', ]
+regret_dic = main(algo_list, target, domain, partition, noise, rounds)
+compare_regret(regret_dic)
