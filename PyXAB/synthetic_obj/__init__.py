3c7cf5f82acf59f1a405b8f8b20c9f1921b89291
Williamlwj <williamleewj@gmail.com>
2023-01-30 18:31:51 -0500

    changed

--- PyXAB/synthetic_obj/__init__.py
+++ PyXAB/synthetic_obj/__init__.py
@@ -1,1 +1,10 @@
-__all__ = ["Ackley", "DifficultFunc", "DoubleSine", "Garland", "Himmelblau.py", "Rastrigin", "Cexample"]
+__all__ = ["Ackley", "DifficultFunc", "DoubleSine", "Garland", "Himmelblau", "Rastrigin", "Cexample"]
+
+
+# from .Ackley import Ackley
+# from .DifficultFunc import DifficultFunc
+# from .DoubleSine import DoubleSine
+# from .Garland import Garland
+# from .Himmelblau import Himmelblau
+# from .Rastrigin import Rastrigin
+# from .Cexample import Cexample
--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,7 +12,6 @@
 #
 import os
 import sys
-import numpy as np
 
 sys.path.insert(0, os.path.abspath('../..'))
 sys.path.insert(0, os.path.abspath('../../PyXAB'))
