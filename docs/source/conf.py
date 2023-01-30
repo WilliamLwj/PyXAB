82ff1e79274ebca589c70b8bfcc8f2a813963fa0
Williamlwj <williamleewj@gmail.com>
2023-01-30 17:49:08 -0500

    new doc

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,7 +31,6 @@ release = '0.0.1'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = ['sphinx.ext.autodoc',
-              'sphinx_code_tabs',
               'sphinx.ext.napoleon',
               'myst_parser'
 ]
