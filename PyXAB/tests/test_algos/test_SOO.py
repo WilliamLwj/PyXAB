150dd20af65826c4350b6ff86fb10219a1cc4cc8
talhz <talhz@pku.edu.cn>
2023-02-27 22:28:33 -0500

    change test file

--- PyXAB/tests/test_algos/test_SOO.py
+++ PyXAB/tests/test_algos/test_SOO.py
@@ -7,21 +7,61 @@ from PyXAB.utils.plot import compare_regret
 import math
 import numpy as np
 import pdb
+import pytest
 
-T = 100
 
-Target = Ackley.Ackley_Normalized()
-# Target = DoubleSine.DoubleSine()
-# Target = Garland.Garland()
-domain = [[0, 1], [0, 1]]
-# domain = [[0, 1]]
-partition = BinaryPartition
-algo = SOO(n=T, h_max=100, domain=domain, partition=partition)
+def test_SOO_1():
+    algo = SOO
+    partition = BinaryPartition
+    with pytest.raises(ValueError):
+        algo = SOO(partition=partition, algo=algo)
 
-for t in range(1, T + 1):
-    point = algo.pull(t)
-    reward = Target.f(point)
-    algo.receive_reward(t, reward)
 
-last_point = algo.get_last_point()
-print(algo.iteration, Target.fmax - Target.f(last_point), last_point)
+def test_SOO_2():
+    algo = SOO
+    domain = [[0, 1]]
+    with pytest.raises(ValueError):
+        algo = SOO(domain=domain, algo=algo)
+
+
+def test_SOO_3():
+    domain = [[0, 1]]
+    partition = BinaryPartition
+    with pytest.raises(ValueError):
+        algo = SOO(domain=domain, partition=partition)
+
+
+def test_SOO_4():
+    T = 100
+    Target = Garland.Garland()
+    domain = [[0, 1]]
+    partition = BinaryPartition
+    algo = SOO(n=T, h_max=100, domain=domain, partition=partition)
+
+    for t in range(1, T + 1):
+        point = algo.pull(t)
+        reward = Target.f(point)
+        algo.receive_reward(t, reward)
+
+    algo.get_last_point()
+    # plot the regret
+    # regret_dic = {"POO": np.array(POO_regret_list)}
+    # compare_regret(regret_dic)
+
+
+def test_SOO_5():
+    T = 100
+    Target = Ackley.Ackley_Normalized()
+    domain = [[0, 1], [0, 1]]
+    partition = BinaryPartition
+    algo = SOO(n=T, h_max=100, domain=domain, partition=partition)
+
+    for t in range(1, T + 1):
+        point = algo.pull(t)
+        reward = Target.f(point)
+        algo.receive_reward(t, reward)
+
+    algo.get_last_point()
+    # plot the regret
+    # regret_dic = {"POO": np.array(POO_regret_list)}
+    # compare_regret(regret_dic)
