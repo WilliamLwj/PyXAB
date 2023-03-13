95d2b5e69cb6ab01577cd878995bd9ebde2cd820
talhz <talhz@pku.edu.cn>
2023-03-12 23:41:56 -0400

    rewrite test file for SequOOL

--- PyXAB/tests/test_algos/test_SequOOL.py
+++ PyXAB/tests/test_algos/test_SequOOL.py
@@ -6,17 +6,60 @@ from PyXAB.utils.plot import compare_regret
 import math
 import numpy as np
 import pdb
+import pytest
 
-T = 100
-Target = Garland.Garland()
-domain = [[0, 1]]
-partition = BinaryPartition
-algo = SequOOL(n=T, domain=domain, partition=partition)
+def test_SequOOL_ValueError_1():
+    partition = BinaryPartition
+    with pytest.raises(ValueError):
+        algo = SequOOL(partition=partition)
 
-for t in range(1, T + 1):
-    point = algo.pull(t)
-    reward = Target.f(point)
-    algo.receive_reward(t, reward)
+def test_SequOOL_ValueError_2():
+    domain = [[0, 1]]
+    with pytest.raises(ValueError):
+        algo = SequOOL(domain=domain)
 
-last_point = algo.get_last_point()
-print(algo.iteration, Target.fmax - Target.f(last_point), last_point)
+def test_SequOOL_DoubleSine():
+    T = 500
+    Target = DoubleSine.DoubleSine()
+    domain = [[0, 1]]
+    partition = BinaryPartition
+    algo = SequOOL(n=T, domain=domain, partition=partition)
+
+    for t in range(1, T + 1):
+        point = algo.pull(t)
+        reward = Target.f(point)
+        algo.receive_reward(t, reward)
+        
+    last_point = algo.get_last_point()
+    print(T, Target.fmax - Target.f(last_point))
+
+def test_SequOOL_Ackley():
+    T = 500
+    Target = Ackley.Ackley_Normalized()
+    domain = [[0, 1], [0, 1]]
+    partition = BinaryPartition
+    algo = SequOOL(n=T, domain=domain, partition=partition)
+
+    for t in range(1, T + 1):
+        point = algo.pull(t)
+        reward = Target.f(point)
+        algo.receive_reward(t, reward)
+        
+    last_point = algo.get_last_point()
+    print(T, Target.fmax - Target.f(last_point))
+    
+def test_SequOOL_Garland():
+    T = 500
+    Target = Garland.Garland()
+    domain = [[0, 1]]
+    partition = BinaryPartition
+    algo = SequOOL(n=T, domain=domain, partition=partition)
+
+    for t in range(1, T + 1):
+        point = algo.pull(t)
+        reward = Target.f(point)
+        algo.receive_reward(t, reward)
+        
+    last_point = algo.get_last_point()
+    print(T, Target.fmax - Target.f(last_point))
+        
